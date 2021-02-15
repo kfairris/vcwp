988fbe9f5229eae169986cd1006f6f6f382c1274
Kenny Fairris <kfairris@gmail.com>
2021-02-15 14:38:59 -0600

    Kenny added some notes

--- home.php
+++ home.php
@@ -3,13 +3,19 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <small>Date Posted:<em>&nbsp;<?php the_time('l, F jS, Y'); ?></em></small>
-            <?php the_excerpt(); ?>
-            <?php endwhile;
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+                    <small>Date Posted:<em>&nbsp;<?php the_time('l, F jS, Y'); ?></em></small>
+
+                    <?php the_excerpt(); ?>
+
+                <?php endwhile;
             else : ?>
-            <p><?php _e('Sorry, this page does not exist.'); ?></p>
+
+                <p><?php _e('Sorry, this page does not exist.'); ?></p>
+
             <?php endif; ?>
         </div>
     </div>
